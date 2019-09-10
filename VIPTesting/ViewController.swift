3ecd307e316fc1841b74fdc473b040ff70cf7dd3
Kevin Farst <farst.6@gmail.com>
2019-09-09 23:06:24 -0700

    Initial Commit

---
+++ VIPTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPTesting
+//
+//  Created by Kevin Farst on 9/9/19.
+//  Copyright © 2019 Kevin Farst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
